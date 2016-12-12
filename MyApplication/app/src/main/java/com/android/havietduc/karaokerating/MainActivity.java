d8b791d913db7fce94c8c5b2dafa6e72b0a9d54b
Duc Haviet <ducviet.ict@gmail.com>
2016-12-12 23:17:06 +0700

    Create Tabhost

--- MyApplication/app/src/main/java/com/android/havietduc/karaokerating/MainActivity.java
+++ MyApplication/app/src/main/java/com/android/havietduc/karaokerating/MainActivity.java
@@ -2,6 +2,7 @@ package com.android.havietduc.karaokerating;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+    }
+
+    private void addControls() {
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+         TabHost.TabSpec tab1 = tabHost.newTabSpec("tab1");
+         tab1.setIndicator("", getResources().getDrawable(R.drawable.tab1));
+         tab1.setContent(R.id.tab1);
+         tabHost.addTab(tab1);
+
+
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("tab2");
+        tab1.setIndicator("", getResources().getDrawable(R.drawable.tab2));
+        tab1.setContent(R.id.tab2);
+        tabHost.addTab(tab2);
     }
 }
